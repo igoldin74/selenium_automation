b85b726d62d29129094f76675c2b47e1cc2d81c7
igoldin74 <33078752+igoldin74@users.noreply.github.com>
2019-09-16 20:21:02 -0500

    Admin login fix

--- test/test_lc_login.py
+++ test/test_lc_login.py
@@ -1,4 +1,4 @@
-from selenium.webdriver.common.keys import Keys
+
 
 def test_litecart_user_login(app):
     app.open_user_login_page()
@@ -10,11 +10,9 @@ def test_litecart_user_login(app):
 
 def test_litecart_admin_login(app):
     app.open_admin_login_page()
-    alert = app.wd.switch_to_alert()
-    alert.send_keys("admin")
-    alert.send_keys(Keys.TAB + "admin")
-    alert.accept()
     app.wd.find_element_by_css_selector("[type='text']").clear()
     app.wd.find_element_by_css_selector("[type='text']").send_keys("admin")
     app.wd.find_element_by_css_selector("[type='password']").send_keys("admin")
     app.wd.find_element_by_name("login").click()
+    assert app.wd.find_element_by_css_selector(".success")
+
